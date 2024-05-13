15b46b2ec9b20b4dd91a83fc71f02eb031f6874e
Surya Prakash Mallah <sfrost488@gmail.com>
2024-05-13 20:33:33 +0530

    created the client side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,9 @@
-import { Box , styled } from "@mui/material";
-// components
-import Header from "./components/Header";
-import InfoHeader from "./components/InfoHeader";
-import Article from "./components/Article";
 
-const Container = styled(Box)`
-  width: 60%;
-  margin : 40px auto 0 auto;
-`
 
-const App = () => {
+export default function App() {
   return (
     <>
-      <Box>
-        <Header />
-        <Container>
-          <InfoHeader />
-          <Article />
-        </Container>
-      </Box>
+    
     </>
-  );
-};
-
-export default App;
+  )
+}
